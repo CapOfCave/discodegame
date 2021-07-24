70b25083b1b4c9c773987864de1f01830079993b
Lars Kecker <lars.kecker2009@gmail.com>
2021-07-24 15:21:41 +0200

    setup: initialize spring project

---
+++ src/main/java/me/kecker/discodegame/DiscodegameApplication.java
@@ -0,0 +1,13 @@
+package me.kecker.discodegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscodegameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscodegameApplication.class, args);
+	}
+
+}
