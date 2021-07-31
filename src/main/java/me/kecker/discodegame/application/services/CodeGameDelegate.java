5b1fdead9e3710073c9cd05f363178e2b64cc9da
Lars Kecker <lars.kecker2009@gmail.com>
2021-07-31 12:32:10 +0200

    refactor(application): rename CodeGameImpl to CodeGameDelegate

--- src/main/java/me/kecker/discodegame/application/services/CodeGameDelegate.java
+++ src/main/java/me/kecker/discodegame/application/services/CodeGameDelegate.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor(onConstructor_ = @Autowired)
-public class CodeGameImpl implements CodeGame {
+public class CodeGameDelegate implements CodeGame {
 
     private final SessionManager sessionManager;
 
